83bcff36c06d2b9e2318c6fccf88d953b395dd90
anton <slonikmak@gmail.com>
2016-10-19 14:45:23 +0200

    new

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -1,7 +1,35 @@
 package controllers;
 
+import javafx.beans.binding.Bindings;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import main.Main;
+import repository.Repository;
+import utils.Utils;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by anton on 16.10.16.
  */
-public class MainController {
+public class MainController implements Initializable{
+    Repository repository;
+
+    @FXML
+    Label timer;
+
+    public MainController(Repository repository){
+        this.repository = repository;
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        timer.textProperty().bind(Bindings.createStringBinding(()->{
+            final long timeMillis = repository.getWholeTime().getValue();
+            return Utils.prepareTime(timeMillis)+" min";
+        }, repository.getWholeTime()));
+    }
 }
